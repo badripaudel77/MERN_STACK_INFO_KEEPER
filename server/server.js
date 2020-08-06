713597fcdde1397b37daa974a3ddcc547634e8c1
badripaudel77 <badripaudel77@gmail.com>
2020-08-06 19:12:18 +0545

    backend setup basic project structure completed

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv')
+
+const authRoutes = require('./routes/auth');
+const contactsRoutes = require('./routes/contacts');
+const usersRoutes = require('./routes/users');
+const connectDB = require('./config/db')
+
+const app = express();
+
+app.use(express.json()) //dont need bodyParser
+
+//load the config file
+dotenv.config({path: './config/config.env' })
+
+const PORT = process.env.PORT || 5000;
+const URL = `http://localhost:/${PORT}`;
+
+connectDB(); 
+app.listen(PORT, () => console.log("Server is listening at PORT : " + URL ));
